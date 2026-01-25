e5c36c953d70e956f1a480b66368264e42c37c17
Shilo <shilo86@gmail.com>
2026-01-25 02:39:51 -0800

    refactor: Update LongPressIcon dimensions and stroke width for improved design
    - Increased the stroke width from 16 to 32 for better visibility.
    - Adjusted the arc path dimensions from 52 to 62 to enhance the icon's representation of long press actions.

--- src/lib/icons/LongPressIcon.svelte
+++ src/lib/icons/LongPressIcon.svelte
@@ -12,12 +12,12 @@
 >
   <rect width="256" height="256" fill="none" />
   <path
-    d="M64,76a52,52,0,0,1,104,0"
+    d="M58,76a62,62,0,0,1,125,0"
     fill="none"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-linejoin="round"
-    stroke-width="16"
+    stroke-width="32"
   />
   <path
     d="M72,224,42.68,174a20,20,0,0,1,34.64-20L96,184V76a20,20,0,0,1,40,0v56a20,20,0,0,1,40,0v16a20,20,0,0,1,40,0v36c0,24-8,40-8,40"
