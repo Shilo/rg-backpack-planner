d20fa0a34b0eb615dd7fa1ffc1f141d58ee8321f
Shilo <shilo86@gmail.com>
2026-03-01 03:04:00 -0800

    chore(tests): reorder test imports
    - Moved the import of "encoder.test.ts" to a different position
      in the test file. This change improves the organization
      of the test imports for better readability.

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 import "./appInfo.test.ts";
-import "./encoder.test.ts";
 import "./editableSurfaceStyles.test.ts";
 import "./globalContextMenu.test.ts";
+import "./encoder.test.ts";
 import "./tierLeveling.test.ts";
