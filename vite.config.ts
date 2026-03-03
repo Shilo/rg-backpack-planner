7062b047ca43d76a299c19d5c06ebbed8a35c5cd
Shilo <shilo86@gmail.com>
2026-03-02 21:53:47 -0800

    feat: pwa icon color and size

--- vite.config.ts
+++ vite.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
                 description: "Plan and share Backpack Tech builds.",
                 start_url: ".",
                 display: "standalone",
-                background_color: "#060b16",
-                theme_color: "#060b16",
+                background_color: "#ffffff",
+                theme_color: "#ffffff",
                 icons: [
                     {
                         src: "pwa-192x192.png",
