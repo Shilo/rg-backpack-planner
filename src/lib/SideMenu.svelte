601eb88f7cf4884e163b752c75b7fde176736b78
Shilo <shilo86@gmail.com>
2026-03-04 04:26:18 -0800

    fix: side menu padding offset

--- src/lib/SideMenu.svelte
+++ src/lib/SideMenu.svelte
@@ -184,8 +184,14 @@
         display: block;
         height: 100%;
         overflow-y: auto;
-        padding: 0 calc(var(--spacing-md) + var(--safe-right, 0px)) 0
-            var(--spacing-md);
+
+        --side-menu-padding-left: var(--spacing-md);
+        --side-menu-padding-right: calc(
+            var(--spacing-md) + var(--safe-right, 0px)
+        );
+
+        padding: 0 var(--side-menu-padding-right) 0
+            var(--side-menu-padding-left);
         scrollbar-gutter: stable;
         scrollbar-width: thin;
     }
@@ -203,14 +209,24 @@
             /* 
                On desktop with classic scrollbars, the visual gap is:
                Base Padding + Scrollbar Gutter. 
-               We subtract the scrollbar width from the padding to keep the total gap exactly --spacing-md.
+               We subtract the scrollbar width from the padding to keep the total gap exactly the same as the variable.
             */
             padding: 0
-                calc(
-                    max(0px, var(--spacing-md) - var(--scrollbar-visual-width)) +
-                        var(--safe-right, 0px)
+                max(
+                    0px,
+                    calc(
+                        var(--side-menu-padding-right) -
+                            var(--scrollbar-visual-width)
+                    )
                 )
-                0 max(0px, var(--spacing-md) - var(--scrollbar-visual-width));
+                0
+                max(
+                    0px,
+                    calc(
+                        var(--side-menu-padding-left) -
+                            var(--scrollbar-visual-width)
+                    )
+                );
         }
 
         .side-menu__content::-webkit-scrollbar {
