096570ba41e8c55b47550d545fc9e88f92700e34
Shilo <shilo86@gmail.com>
2026-03-28 22:54:53 -0700

    feat: new stun and ignore stun icons

--- src/assets/nodes/IgnoreStun.svelte
+++ src/assets/nodes/IgnoreStun.svelte
@@ -9,7 +9,7 @@
     let props: SVGAttributes<SVGSVGElement> = $props();
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" preserveAspectRatio="xMidYMid meet" {...props}>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="28 38 200 180" preserveAspectRatio="xMidYMid meet" {...props}>
     <defs>
         <mask
             id={swirlCutMaskId}
@@ -21,12 +21,12 @@
         >
             <rect width="256" height="256" fill="white" />
             <line
-                x1="30"
-                y1="30"
-                x2="226"
-                y2="226"
+                x1="55"
+                y1="62"
+                x2="196"
+                y2="203"
                 stroke="black"
-                stroke-width="32"
+                stroke-width="46"
                 stroke-linecap="round"
             />
         </mask>
@@ -34,16 +34,16 @@
     <path
         fill="currentColor"
         mask={`url(#${swirlCutMaskId})`}
-        d="M250 138a12 12 0 0 1-24 0a94 94 0 0 0-94-94a84 84 0 0 0-84 84a74 74 0 0 0 74 74a64 64 0 0 0 64-64a54 54 0 0 0-54-54a44 44 0 0 0-44 44a34 34 0 0 0 34 34a24 24 0 0 0 24-24a14 14 0 0 0-14-14a4 4 0 0 0-2.83 1.17A12 12 0 0 1 124 148a20 20 0 0 1-20-20a28 28 0 0 1 28-28a38 38 0 0 1 38 38a48 48 0 0 1-48 48a58 58 0 0 1-58-58a68 68 0 0 1 68-68a78 78 0 0 1 78 78a88 88 0 0 1-88 88a98 98 0 0 1-98-98A108 108 0 0 1 132 20a118 118 0 0 1 118 118"
+        d="M223 143a10 10 0 0 1-20 0a80 80 0 0 0-80-80a70 70 0 0 0-70 70a60 60 0 0 0 60 60a50 50 0 0 0 50-50a40 40 0 0 0-40-40a30 30 0 0 0-30 30a20 20 0 0 0 20 20a10 10 0 0 0 10-10a10 10 0 0 1 0-20a20 20 0 0 1 20 20a30 30 0 0 1-30 30a40 40 0 0 1-40-40a50 50 0 0 1 50-50a60 60 0 0 1 60 60a70 70 0 0 1-70 70a80 80 0 0 1-80-80a90 90 0 0 1 90-90a100 100 0 0 1 100 100"
     />
     <line
-        x1="30"
-        y1="30"
-        x2="226"
-        y2="226"
+        x1="55"
+        y1="62"
+        x2="196"
+        y2="203"
         fill="none"
         stroke="currentColor"
-        stroke-width="20"
+        stroke-width="16"
         stroke-linecap="round"
     />
 </svg>
